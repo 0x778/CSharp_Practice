c548e1e42696cfe5185e9a4b9c240b25ac0f4181
00x78 <hussamalmaswari@gmail.com>
2024-04-15 00:52:17 +0300

    First Practice with system files

---
+++ DotNetFiles/Practice_1/Program.cs
@@ -0,0 +1,35 @@
+﻿    using System.IO;
+//Return Directores
+    IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+
+    foreach (var dir in listOfDirectories) {
+        Console.WriteLine(dir);
+    }
+    Console.WriteLine("====================================================");
+
+// Outputs:
+// stores/201
+// stores/202
+
+///////////////////////////////////////////////////////////////
+//return with Files
+IEnumerable<string> files = Directory.EnumerateFiles("stores");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+    Console.WriteLine("====================================================");
+
+// Find all *.txt files in the stores folder and its subfolders
+IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in allFilesInAllFolders)
+{
+    Console.WriteLine(file);
+}
+    Console.WriteLine("====================================================");
+
+// Outputs:
+// stores/totals.txt
+// stores/201/inventory.txt
